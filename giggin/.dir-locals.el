72b13eb53dce581ad738708b6f529522ee3b6857
Simon Katz <sk@nomistech.com>
2019-10-01 16:42:43 +0100

    Add `cider-repl-history-file` to .dir-locals.el

--- giggin/.dir-locals.el
+++ giggin/.dir-locals.el
@@ -2,4 +2,6 @@
          (cider-shadow-default-options . ":app")
          (nomis/cider-cljs-offer-to-open-app-in-browser? . nil)
          (nomis/rgrep-local-ignored-directories . ("giggin/public/js"
-                                                   "increments")))))
+                                                   "increments"))))
+ (cider-repl-mode
+  (cider-repl-history-file . ".cider-repl-history")))
