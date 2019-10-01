92228a3dfd7ecf6844ab310826e249a7efea8e39
Simon Katz <sk@nomistech.com>
2019-10-01 13:16:15 +0100

    Add .dir-locals.el to simplify dev in Emacs

---
+++ giggin/.dir-locals.el
@@ -0,0 +1,5 @@
+((nil . ((cider-default-cljs-repl . shadow)
+         (cider-shadow-default-options . ":app")
+         (nomis/cider-cljs-offer-to-open-app-in-browser? . nil)
+         (nomis/rgrep-local-ignored-directories . ("giggin/public/js"
+                                                   "increments")))))
