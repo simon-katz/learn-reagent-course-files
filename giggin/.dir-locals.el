bab978ee29318a3a3832250e892920f7ed3f213e
Simon Katz <sk@nomistech.com>
2019-10-03 13:46:57 +0100

    Remove `cider-repl-history-file` from .dir-locals.el
    You can set this to a relative path in your Emacs init, and that works
    relative to the REPL's path.

--- giggin/.dir-locals.el
+++ giggin/.dir-locals.el
@@ -2,6 +2,4 @@
          (cider-shadow-default-options . ":app")
          (nomis/cider-cljs-offer-to-open-app-in-browser? . nil)
          (nomis/rgrep-local-ignored-directories . ("giggin/public/js"
-                                                   "increments"))))
- (cider-repl-mode
-  (cider-repl-history-file . ".cider-repl-history")))
+                                                   "increments")))))
