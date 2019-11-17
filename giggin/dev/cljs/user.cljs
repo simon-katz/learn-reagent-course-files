08b18ab2dff4fad8eafcf2cc4da3fced47fab343
Simon Katz <sk@nomistech.com>
2019-11-17 21:35:15 +0000

    Add `cljs.user` ns

---
+++ giggin/dev/cljs/user.cljs
@@ -0,0 +1,9 @@
+(ns cljs.user
+  "Namespace to support hacking at the CLJS REPL.")
+
+;;;; ___________________________________________________________________________
+;;;; cljs dev stuff
+
+(comment
+  ;; To re-render after evaluating forms and without saving files:
+  (giggin.core/main))
