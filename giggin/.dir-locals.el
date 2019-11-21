e29a94d648addf07d70727b233a93737dbcf3381
Simon Katz <sk@nomistech.com>
2019-11-21 13:52:33 +0000

    Add dir-locals for CIDER CLJS connection
    Add `nomis/cider/cljs-dev-host` and `nomis/cider/cljs-dev-port`
    to dir-locals.

--- giggin/.dir-locals.el
+++ giggin/.dir-locals.el
@@ -1,5 +1,7 @@
 ((nil . ((cider-default-cljs-repl . shadow)
          (cider-shadow-default-options . ":app")
+         (nomis/cider/cljs-dev-host . "localhost")
+         (nomis/cider/cljs-dev-port . 3333)
          (nomis/cider-cljs-offer-to-open-app-in-browser? . nil)
          (nomis/rgrep-local-ignored-directories . ("giggin/public/js"
                                                    "increments")))))
